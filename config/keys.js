639aa03ee215f87b3d72c57fae774c9b023a6530
thasquirrie <thasquirrie@gmail.com>
2020-01-02 07:48:53 +0100

    Changed the database URL

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://thasquirrie:bigdad12@ds021462.mlab.com:21462/heroku_v5663z9d',
-  googleClientID: '1079908353100-ltf2toeoubfh3fqg50ej5jp703itv32n.apps.googleusercontent.com',
-  googleClientSecret: 'V2bk8DxajYXnLDdA3_ennLI8'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
